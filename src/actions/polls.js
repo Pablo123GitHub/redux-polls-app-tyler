342d40154d6f343b2b821eb6ccd8583d22409505
Pablo <paul.ladiv123@gmail.com>
2018-08-03 19:01:09 +0100

    create action creators files and import them in shared.js to handle initial data in store

---
+++ src/actions/polls.js
@@ -0,0 +1,10 @@
+
+
+export const RECEIVE_POLLS = 'RECEIVE_POLLS'
+
+export function receivePolls(polls) {
+    return {
+        type: RECEIVE_POLLS,
+        polls
+    }
+}
---
+++ src/actions/shared.js
@@ -0,0 +1,20 @@
+import { getInitialData} from "../utils/api"
+import { receivePolls} from "./polls"
+import { receiveUsers} from "./users"
+import { setAuthedUser} from "./authedUser"
+
+const AUTHED_ID = 'sarah_edo'
+
+export function handleInitialData () {
+    return (dispatch) => {
+        return getInitialData()
+            .then(({ users, polls})=>{
+                dispatch(receiveUsers(users))
+                dispatch(receivePolls(polls))
+                dispatch(setAuthedUser(AUTHED_ID))
+            })
+    }
+
+}
+
+
