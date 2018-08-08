e38c5419dc8a7384d9e4b4b0cdec364de5ad502f
Pablo <paul.ladiv123@gmail.com>
2018-08-08 11:02:49 +0100

    add loading bar from react-redux-loading package

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,12 @@ import { combineReducers} from 'redux'
 import authedUser from './auhedUsers'
 import users from './users'
 import polls from './polls'
+import { loadingBarReducer } from 'react-redux-loading'
+
 
 export default combineReducers({
     authedUser,
     users,
     polls,
+    loadingBar: loadingBarReducer
 })
